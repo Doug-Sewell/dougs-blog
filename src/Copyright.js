e0e23ba2da64c9067205200c4d088e8d5403da29
the computer <DouglasASewell@gmail.com>
2021-09-25 00:41:25 -0400

    Copyright now utilizes Next JS links to prevent the entire DOM from re-rendering.

--- src/Copyright.js
+++ src/Copyright.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
-import MuiLink from '@mui/material/Link';
+import Link from './Link';
 
 export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <MuiLink color="inherit" href="/">
+      <Link color="inherit" href="/">
         Doug's Blog
-      </MuiLink>{' '}
+      </Link>{' '}
       {new Date().getFullYear()}.
     </Typography>
   );
